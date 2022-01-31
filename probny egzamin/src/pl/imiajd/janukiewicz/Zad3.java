f68bd062de1f6421c2284436f9584016fe9c4cb6
Alan <ayyjay1357@gmail.com>
2022-01-31 01:09:59 +0100

    update

--- probny egzamin/src/pl/imiajd/janukiewicz/Zad3.java
+++ probny egzamin/src/pl/imiajd/janukiewicz/Zad3.java
@@ -1,4 +1,13 @@
 package pl.imiajd.janukiewicz;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Zad3 {
+    public static String[] directoryList(String path, String extension){
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        File f = new File(path);
+        return f.list((dir, name) -> !name.endsWith(extension.toUpperCase()));
+    }
 }
